830dff26f0e1c7fc3263f4c886ef090836219df9
unknown <butlermuwo@gmail.com>
2022-06-17 12:47:12 +0200

    Add details reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import courseReducer from './coursesSlice';
+import detailsReducer from './detailsSlice';
 
 const store = configureStore({
   reducer: {
     course: courseReducer,
+    detail: detailsReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
