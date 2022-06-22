0aea53ee4310b8fd00ffbb53e6e637fef2bb304b
unknown <butlermuwo@gmail.com>
2022-06-22 23:27:45 +0200

    Fix linter errors

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,23 +1,22 @@
-import React, { useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux';
-import { NavLink } from 'react-router-dom';
-import link from './Link';
-import './Navbar'
+// import React, { useState } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { NavLink } from 'react-router-dom';
+// import link from './Link';
+// import './Navbar';
 
+// function Navbar() {
+//   const [sidebar, setSidebar] = useState(false);
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
+//   const user = useSelector((state) => state.userReducer.userDetails);
+//   const showSidebar = () => setSidebar(!sidebar);
+//   const getClass = ({ isActive }) => (isActive ? 'active' : '');
 
-function Navbar() {
-    const [sidebar, setSidebar] = useState(false);
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const user = useSelector((state) => state.userReducer.userDetails);
-    const showSidebar = () => setSidebar(!sidebar);
-    const getClass = ({ isActive }) => (isActive ? 'active' : '');
+//   return (
+//     <>
+//       <div />
+//     </>
+//   );
+// }
 
-  return (
-    <>
-        <div></div>
-    </>
-  )
-}
-
-export default Navbar
+// export default Navbar;
