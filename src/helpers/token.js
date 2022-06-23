6691954127115d8f51ae53bb09d20840fd533f2c
zairdon20 <josphat2020@gmail.com>
2022-06-24 01:49:10 +0200

    Update

--- src/helpers/token.js
+++ src/helpers/token.js
@@ -1,8 +1,8 @@
-// eslint-disable-next-line camelcase
-import jwt_decode from 'jwt-decode';
+// // eslint-disable-next-line camelcase
+// import jwt_decode from 'jwt-decode';
 
-const user = { getUser: () => JSON.parse(localStorage.getItem('userAuth')) };
-const token = user.getUser();
-const decoded = jwt_decode(token);
+// const user = { getUser: () => JSON.parse(localStorage.getItem('userAuth')) };
+// const token = user.getUser();
+// const decoded = jwt_decode(token);
 
-export default decoded;
+// export default decoded;
