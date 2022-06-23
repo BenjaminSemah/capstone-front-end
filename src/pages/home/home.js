d7d7926e45a740d000909fafb09deb45acb8d4b4
sneeu-leeu <sa070197@gmail.com>
2022-06-23 22:46:00 +0200

    Feature: Add to homepage

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -14,7 +14,7 @@ const Home = () => {
             To see this page you need to
           </h2>
           <h3>
-            <Link to="/login" className="mt-2" style={{ textDecoration: 'none' }}>
+            <Link to="/login" style={{ textDecoration: 'none' }}>
               <span>Login</span>
             </Link>
           </h3>
