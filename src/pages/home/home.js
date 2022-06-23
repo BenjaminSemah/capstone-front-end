7c03b852de784c25dc385d1533c3d57e9d9430bb
sneeu-leeu <sa070197@gmail.com>
2022-06-23 21:07:04 +0200

    FFFFix: remove commented code

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,7 +1,5 @@
-/*eslint-disable*/
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
 import Pagination from '../../components/pagination';
 import './home.css';
 
