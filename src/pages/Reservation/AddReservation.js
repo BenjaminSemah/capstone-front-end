c2313bc86b9937151b1bbcdbd11eb12968f41a8c
zairdon20 <josphat2020@gmail.com>
2022-06-24 10:02:52 +0200

    Add the reservations page

--- src/pages/Reservation/AddReservation.js
+++ src/pages/Reservation/AddReservation.js
@@ -1,18 +1,22 @@
+/* eslint-disable react/prop-types */
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+// import { useParams } from 'react-router-dom';
 import { createReservation } from '../../redux/reservationSlice';
-import decoded from '../../helpers/token';
 
-const AddReservation = () => {
+const AddReservation = ({ item }) => {
   const dispatch = useDispatch();
   const [city, setCity] = useState('');
   const [date, setDate] = useState('');
 
+  const coursed = item?.filter((course) => course.id);
+  console.log(coursed);
+  const currentUser = useSelector((state) => state.currentUserr.current);
   const createReserve = () => {
     dispatch(createReservation({
       city,
       date,
-      user_id: decoded.sub,
+      user_id: currentUser.id,
       course_id: 1,
     }));
   };
