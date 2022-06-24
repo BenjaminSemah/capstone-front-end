6fc46712c195c2c3ac408a3eb301071bdf3d8b0c
sneeu-leeu <sa070197@gmail.com>
2022-06-24 08:49:05 +0200

    Fix: undo test

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -6,23 +6,23 @@ import './home.css';
 const Home = () => {
   const authuser = localStorage.getItem('userAuth');
   return (
-    // <>
-    //   {!authuser ? (
-    //     <div className="text-center my-4">
-    //       <h2>
-    //         {' '}
-    //         To see this page you need to
-    //       </h2>
-    //       <h3>
-    //         <Link to="/login" style={{ textDecoration: 'none' }}>
-    //           <span>Login</span>
-    //         </Link>
-    //       </h3>
-    //     </div>
-    //   ) : (
-    //     <Pagination />
-    //   )}
-    // </>
+    <>
+      {!authuser ? (
+        <div className="text-center my-4">
+          <h2>
+            {' '}
+            To see this page you need to
+          </h2>
+          <h3>
+            <Link to="/login" style={{ textDecoration: 'none' }}>
+              <span>Login</span>
+            </Link>
+          </h3>
+        </div>
+      ) : (
+        <Pagination />
+      )}
+    </>
   );
 };
 
