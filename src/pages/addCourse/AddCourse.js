f132113dec9045769c3f54ec19e6ea279ed14c85
omarsalem7 <omarsalem0721@gmail.com>
2022-06-24 16:46:13 +0200

    fix bug

--- src/pages/addCourse/AddCourse.js
+++ src/pages/addCourse/AddCourse.js
@@ -5,23 +5,23 @@ import './AddCourse.css';
 
 export default function AddCourse() {
   const currentUser = useSelector((state) => state.currentUserr.current);
+  const userToken = JSON.parse(localStorage.getItem('userAuth'));
+  if (!userToken || !currentUser.admin) {
+    return (
+      <div className="text-center mt-5">
+        <h2 className="text-danger">You Are Not Authorised To Add a Course</h2>
+        <h3 className="my-4">Please Log in as Admin</h3>
+      </div>
+    );
+  }
   return (
     <div className="pt-5">
-      {!currentUser.admin ? (
-        <div className="text-center">
-          <h2 className="text-danger">
-            You Are Not Authorised To Add a Course
-          </h2>
-          <h3 className="my-4">Please Log in as Admin</h3>
+      <div className="newCourse--form">
+        <div className="add--course">
+          <h1 className="add--title">Add a New Course</h1>
+          <AddCourseForm />
         </div>
-      ) : (
-        <div className="newCourse--form">
-          <div className="add--course">
-            <h1 className="add--title">Add a New Course</h1>
-            <AddCourseForm />
-          </div>
-        </div>
-      )}
+      </div>
     </div>
   );
 }
